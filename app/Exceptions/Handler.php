2a88f0fb6471aeb46f0c36063dff3bc702ef0edb
Dimas Bayu Sampurno <dimasbayu.smprno1106@gmail.com>
2024-03-12 22:11:07 +0800

    adjust : exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,29 +2,68 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Throwable;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * The list of the inputs that are never flashed to the session on validation exceptions.
-     *
-     * @var array<int, string>
-     */
     protected $dontFlash = [
         'current_password',
         'password',
         'password_confirmation',
     ];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     */
+    protected $dontReport = [
+        // AuthorizationException::class,
+        // HttpException::class,
+        // ModelNotFoundException::class,
+        // ValidationException::class
+    ];
+
     public function register(): void
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->reportable(function (\Throwable $e) {
+          
         });
+    }
+
+
+    public function render($request, \Throwable $e)
+    {
+        if ($e instanceof ModelNotFoundException) {
+            return response()->json([
+                'message' => 'Ga ketemu nih!',
+                'status' => 'error'
+            ], 400);
+        }
+
+        if (
+            !($e instanceof ValidationException) &&
+            !($e instanceof NotFoundHttpException) &&
+            (bool) env('Exception_Status', true)
+        ) {
+            if (app()->environment('production')) {
+                // Simpan informasi kesalahan ke dalam database
+                // DB::connection('mysql')
+                //     ->table('exception_logs')
+                //     ->insert([
+                //         'endpoint' => $request->fullUrl(),
+                //         'path' => substr($request->path(), 0, 190),
+                //         'payload' => json_encode($request->all()),
+                //         'error_message' => substr($e->getMessage(), 0, 190),
+                //         'exception' => $e->__toString(),
+                //         'logged_at' => now()
+                //     ]);
+
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Internal Server Error',
+                ], 500);
+            } 
+        }
+
+        return parent::render($request, $e);
     }
 }
