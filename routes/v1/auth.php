1d724cbc6a1f646e844e18b78f3c5c09dad9c9ee
Dimas Bayu Sampurno <dimasbayu.smprno1106@gmail.com>
2024-03-12 22:05:25 +0800

    update : implement response helper

--- routes/v1/auth.php
+++ routes/v1/auth.php
@@ -3,4 +3,5 @@ use App\Http\Controllers\Api\Apicontroller;
 
 Route::prefix('cms')->group(function ($route) {
     $route->post('register', Apicontroller::class . '@register');
+    $route->post('login', Apicontroller::class . '@login');
 });
